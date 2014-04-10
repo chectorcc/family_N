cd2cd82455553bad3ee8875089dda96700d30563
Hector Calderon <chinohec@gmail.com>
2014-04-10 14:01:13 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FamilyN::Application.config.secret_key_base = '85e00ab5d2fc1cbaee4dc0499993aa0854ea858152258eb1279b9ea0fab99adf9e716c05f9fd2a2d0dbe165a3fa78711f7f1c12c8eccb21b365ed3c62aee4ad1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FamilyN::Application.config.session_store :cookie_store, key: '_family_N_session'
